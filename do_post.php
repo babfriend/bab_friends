1fbcd2a5628e18f5f55561cc41cf39894f70a34f
maythutun <blackrainbow93@gmail.com>
2014-05-23 01:47:21 +0900

    Create do_post.php

---
+++ do_post.php
@@ -0,0 +1,25 @@
+<?php
+if((!$_POST["title"]) || (!$_POST["content"])){
+	header("location: addpost.html");
+	exit;
+}
+
+$mysqli = mysqli_connect("localhost", "root", "brightstar1993","bab_friends");
+$addpost_sql = "INSERT INTO `post` (`title`, `post_time`, `location`, `content`, `menu`) VALUES (' ".$_POST["title"]." ', now( ),  '".$_POST["location"]."', '".$_POST["content"]."', '".$_POST["menu"]."')";
+
+$addpost_res = mysqli_query($mysqli, $addpost_sql) or 
+die (mysqli_error($mysqli));
+
+$post_id = mysqli_insert_id($mysqli);
+mysqli_close($mysqli);
+
+$display_block = "<P>The <strong>".$_POST["title"]."</strong> post has been created.</p>";
+?>
+<html>
+<head>
+<title>New Post Added</title>
+</head>
+<body>
+<?php echo $display_block; ?>
+</body>
+</html>
